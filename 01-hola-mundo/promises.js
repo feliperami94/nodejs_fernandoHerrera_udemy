01a06ded9694644623d83390dd06e07c0286f11f
Felipe Ramirez <felipe.ramirezrivera@gmail.com>
2022-08-02 16:36:03 -0500

    Created the 'getSalario' function with promise

--- 01-hola-mundo/promises.js
+++ 01-hola-mundo/promises.js
@@ -25,21 +25,35 @@ const salarios = [
 ];
 
 const getEmpleado = (id) => {
-  const promesa = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     
     const empleado = empleados.find((e) => e.id === id)?.nombre;
-    
+
     (empleado)
       ? resolve(empleado)
       : reject(`No existe empleado con id ${id}`);
 
   });
 
-  return promesa;
 };
 
-const id = 2;
+const getSalario = (id) => {
+    return new Promise((resolve, reject) => {
+
+        const salario = salarios.find(sal =>sal.id === id)?.salario;
+
+        (salario)
+            ? resolve(salario)
+            : reject(`El salario del id ${id} no existe`);
+    })
+}
+
+const id = 3;
 
 getEmpleado(id)
   .then((empleado) => console.log(empleado))
   .catch( err => console.log(err));
+
+  getSalario(id)
+  .then( salario => console.log(salario))
+  .catch(err => console.log(err));
