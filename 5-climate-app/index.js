65497deab7b5c3568d47e4f15e0023d1e3a87470
feliperami94 <felipe.ramirezrivera@gmail.com>
2022-08-09 10:47:54 -0500

    Advanced in the Find City option of the menu

--- 5-climate-app/index.js
+++ 5-climate-app/index.js
@@ -1,11 +1,41 @@
 const { readInput, inquirerMenu, pausa } = require("./helpers/inquirer")
+const colors = require('colors');
+const { Searches } = require("./models/searches");
 
 
 const main = async () => {
+    
+    const searches = new Searches();
     let opt;
+    
+
 
     do {
         opt = await inquirerMenu();
+        
+        switch(opt){
+
+            case 1:
+                const placeSearch = await readInput('City: ');
+                console.log(placeSearch);
+                console.log('\nInfo of the city\n'.green)
+                console.log('City: ',);
+                console.log('Lat: ',);
+                console.log('Lng: ',);
+                console.log('Temperature: ',);
+                console.log('Min: ',);
+                console.log('Max: ',);
+
+
+
+
+
+            break;
+
+            case 2:
+
+        }
+
         await pausa();
         
     } while (opt!==0);
--- 5-climate-app/models/searches.js
+++ 5-climate-app/models/searches.js
@@ -7,8 +7,14 @@ class Searches {
         //TODO Leer DB
     }
 
+    async city(place = ''){
+        console.log(place) //HTTP Request
+
+        return []; //Return the places that match the search
+    }
+
 }
 
-module.export = {
+module.exports = {
     Searches
 }
